59f0cb24fb7866cfc60e9798da7f8c3f167b71d0
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-08-22 12:51:21 +0300

    html

--- app.js
+++ app.js
@@ -10,5 +10,26 @@ fetch(
       const coinInfo = json[`${coin}`];
       const price = coinInfo.usd;
       const change = coinInfo.usd_24_change.toFixed(5);
+
+      container.innerHTML += `
+      <div class="coin falling">
+        <div class="coin-logo">
+          <img src="images/bitcoin.png" alt="bitcoin" />
+        </div>
+        <div class="coin-name">
+          <h3>bitcoin</h3>
+          <span>/USD</span>
+        </div>
+        <div class="coin-price">
+          <span class="price">$23521</span>
+          <span class="change">-1.022</span>
+        </div>
+      </div>
+      
+      
+      
+      
+      
+      `;
     }
   });
