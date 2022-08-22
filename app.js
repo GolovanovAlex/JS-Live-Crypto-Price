b1921899061bd236ee386cbb0c0b080348e7d25b
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-08-22 12:59:28 +0300

    js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 fetch(
-  'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Ccardano%2Cdogecoin%2Cethereum%2Clitecoin%2Ctether%2Cripple&vs_currencies=usd&include_24hr_change=true'
+  'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin%2Ccardano%2Ctether&vs_currencies=usd&include_24hr_change=true'
 )
   .then((res) => res.json())
   .then((json) => {
@@ -12,24 +12,20 @@ fetch(
       const change = coinInfo.usd_24_change.toFixed(5);
 
       container.innerHTML += `
-      <div class="coin falling">
-        <div class="coin-logo">
-          <img src="images/bitcoin.png" alt="bitcoin" />
+        <div class="coin ${change < 0 ? 'falling' : 'rising'} ">
+          <div class="coin-logo">
+                <img src="images/${coin}.png" alt="bitcoin" />
+          </div>
+              <div class="coin-name">
+                <h3>${coin}</h3>
+                <span>/USD</span>
+              </div>
+              <div class="coin-price">
+                <span class="price">${price}</span>
+                <span class="change">${change}</span>
+              </div>
         </div>
-        <div class="coin-name">
-          <h3>bitcoin</h3>
-          <span>/USD</span>
-        </div>
-        <div class="coin-price">
-          <span class="price">$23521</span>
-          <span class="change">-1.022</span>
-        </div>
-      </div>
-      
-      
-      
-      
-      
+    
       `;
     }
   });
